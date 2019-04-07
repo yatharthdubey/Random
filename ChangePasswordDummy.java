ad664d5d17c63843836b1822c002247c94f15660
yatharthdubey <46920224+yatharthdubey@users.noreply.github.com>
2019-04-07 20:02:15 +0530

    Add files via upload

---
+++ ChangePasswordDummy.java
@@ -0,0 +1,37 @@
+package com.capgemini.capstore.beans;
+
+public class ChangePasswordDummy {
+
+	private String email;
+	private String oldPassword;
+	private String newPassword;
+	private String role;
+	
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getOldPassword() {
+		return oldPassword;
+	}
+	public void setOldPassword(String oldPassword) {
+		this.oldPassword = oldPassword;
+	}
+	public String getNewPassword() {
+		return newPassword;
+	}
+	public void setNewPassword(String newPassword) {
+		this.newPassword = newPassword;
+	}
+	
+	
+	
+}
